cfb003981bb9a5a7b8bb44002e2e0ce048927c65
William Hicks <will.hicks917@gmail.com>
2017-07-11 23:47:25 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,13 @@ class Config(object):
 	DEVELOPMENT = True 
 	SECRET_KEY = '@!secretkey!'
 	static_folder = 'static'
+	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://super:!important@inqw-442.postgres.pythonanywhere-services.com:10442/sms'
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class Auth(object):
+	CLIENT_ID = ('KEY')
+	CLIENT_SECRET = SECRET'
+	REDIRECT_URI = 'https://localhost:5000/authorize/google'
+	AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
+	TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
+	USER_INFO = 'https://www.googleapis.com/userinfo/v2/me'
